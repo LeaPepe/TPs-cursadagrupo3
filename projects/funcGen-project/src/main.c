ec3a73e0fe5b58268338e557ff4707af9555f298
LeaPepe <leapalazzo@gmail.com>
2020-11-07 19:36:15 -0300

    funcGen probado en placa, el resto solo statechart

---
+++ projects/funcGen-project/src/main.c
@@ -0,0 +1,147 @@
+#include "main.h"
+
+
+// overridable tick flag
+volatile bool SysTick_Time_Flag = false;
+
+// State machine global statechart
+static FuncGen statechart;
+
+// Timer ticks
+TimerTicks ticks[NOF_TIMERS];
+
+// state machine user-defined external functions (action)
+void funcGenIface_opAddV(const FuncGen* handle){
+	Board_LED_Set(LED1, TRUE);
+	Board_LED_Set(LED3, !Board_LED_Test(LED3)); // swap state
+}
+void funcGenIface_opSubV(const FuncGen* handle){
+	Board_LED_Set(LED1, TRUE);
+	Board_LED_Set(LED2, !Board_LED_Test(LED2)); // swap state
+}
+void funcGenIface_opAddF(const FuncGen* handle){
+	Board_LED_Set(LED1, FALSE);
+	Board_LED_Set(LED3, !Board_LED_Test(LED3)); // swap state
+}
+void funcGenIface_opSubF(const FuncGen* handle){
+	Board_LED_Set(LED1, FALSE);
+	Board_LED_Set(LED2, !Board_LED_Test(LED2)); // swap state
+}
+void funcGenIface_opSetForm(const FuncGen* handle, const sc_integer form){
+	switch(form){
+	    case OPT_SQR:
+	    	Board_LED_Set(LED0_R, TRUE);
+	    	Board_LED_Set(LED0_G, FALSE);
+	    	Board_LED_Set(LED0_B, FALSE);
+	      break;
+	    case OPT_SIN:
+	    	Board_LED_Set(LED0_R, FALSE);
+	    	Board_LED_Set(LED0_G, TRUE);
+	    	Board_LED_Set(LED0_B, FALSE);
+	      break;
+	    case OPT_TRI:
+	    	Board_LED_Set(LED0_R, FALSE);
+			Board_LED_Set(LED0_G, FALSE);
+			Board_LED_Set(LED0_B, TRUE);
+	    default:
+	    	break;
+	      // default statements
+	}
+}
+
+// Scan for all buttons
+uint32_t Buttons_GetStatus_(void) {
+	uint8_t ret = false;
+	uint32_t idx;
+
+	for (idx = 0; idx < 4; ++idx) {
+		if (Board_TEC_GetStatus(BOARD_TEC_1 + idx) == 0)
+			ret |= 1 << idx;
+	}
+	return ret;
+}
+
+// Set time services for state machine
+void funcGen_setTimer( FuncGen* handle, const sc_eventid evid, const sc_integer time_ms, const sc_boolean periodic )
+{
+	SetNewTimerTick(ticks, NOF_TIMERS, evid, time_ms, periodic);
+}
+
+// Unset time services for state machine
+void funcGen_unsetTimer( FuncGen* handle, const sc_eventid evid )
+{
+	UnsetTimerTick( ticks, NOF_TIMERS, evid );
+}
+
+// Set the flag from interruption (every 1ms)
+void SysTick_Handler(void){
+	SysTick_Time_Flag = true;
+}
+
+// Main
+int main(void)
+{
+	uint32_t i;
+
+	// Button status
+	uint32_t BUTTON_Status;
+
+	// Generic Initialization
+	Board_Init();
+
+	// Init Ticks counter => TICKRATE_MS
+	SysTick_Config(SystemCoreClock / TICKRATE_MS);
+
+	// Init Timer Ticks
+	InitTimerTicks( ticks, NOF_TIMERS );
+
+	// Statechart Initialization
+	funcGen_init( &statechart );
+	funcGen_enter( &statechart );
+
+	// Main while
+	while (1) {
+		// Wait for interruption
+		__WFI();
+
+		// If interruption because timer
+		if (SysTick_Time_Flag == true) {
+
+			// reset flag
+			SysTick_Time_Flag = false;
+
+			// Update timers
+			UpdateTimers( ticks, NOF_TIMERS );
+
+			// Scan all timers
+			for (i = 0; i < NOF_TIMERS; i++) {
+
+				// If pending events
+				if (IsPendEvent( ticks, NOF_TIMERS, ticks[i].evid ) == true) {
+
+					// Raise event
+					funcGen_raiseTimeEvent( &statechart, ticks[i].evid );
+
+					// Mark as atached
+					MarkAsAttEvent( ticks, NOF_TIMERS, ticks[i].evid );
+				}
+			}
+
+			// Get button statuses
+			BUTTON_Status = Buttons_GetStatus_();
+
+
+			// If pressed buttons
+			if (BUTTON_Status != 0)
+				// Raise the pressed event with the status
+				funcGenIface_raise_evTecPressed(&statechart, BUTTON_Status);
+			else
+				// Else raise not pressed event
+				funcGenIface_raise_evTecNotPressed(&statechart);
+
+			// Run state machine cycle
+			funcGen_runCycle(&statechart);
+		}
+	}
+} // Main end
+
---
+++ projects/mecStair-project/src/main.c
@@ -0,0 +1,198 @@
+
+#include "main.h"
+#include "sapi.h"       // <= sAPI header
+#include "FuncGen.h"
+#include "TimerTicks.h"
+
+
+#define TICKRATE_1MS		   (1000)				/* 1000 ticks per second */
+#define TICKRATE_MS			(TICKRATE_1MS)	/* 1000 ticks per second */
+
+
+volatile bool SysTick_Time_Flag = false;
+
+/*! This is a state machine */
+static FuncGen statechart;
+
+/*! This is a timed state machine that requires timer services */
+#define NOF_TIMERS (sizeof(FuncGenTimeEvents)/sizeof(sc_boolean))
+
+TimerTicks ticks[NOF_TIMERS];
+
+
+/*! \file This header defines prototypes for all functions that are required
+ *  by the state machine implementation.
+ *
+ *  This is a state machine uses time events which require access to a timing
+ *  service. Thus the function prototypes:
+ *  - funcGen_setTimer and
+ *  - funcGen_unsetTimer
+ *  are defined.
+ *
+ *  This state machine makes use of operations declared in the state machines
+ *  interface or internal scopes. Thus the function prototypes:
+ *  - funcGenIface_opLED
+ *  are defined.
+ *
+ *  These functions will be called during a 'run to completion step' (runCycle)
+ *  of the statechart.
+ *  There are some constraints that have to be considered for the
+ *  implementation of these functions:
+ *  - never call the statechart API functions from within these functions.
+ *  - make sure that the execution time is as short as possible.
+ */
+
+#define LED0_R    0
+#define LED0_G    1
+#define LED0_B    2
+#define LED1      3
+#define LED2      4
+#define LED3      5
+
+
+/** state machine user-defined external function (action)
+*/
+void funcGenIface_opAddV(const FuncGen* handle){
+	Board_LED_Set(LED1, TRUE);
+}
+void funcGenIface_opSubV(const FuncGen* handle){
+	Board_LED_Set(LED2, TRUE);
+}
+void funcGenIface_opAddF(const FuncGen* handle){
+	Board_LED_Set(LED3, TRUE);
+}
+void funcGenIface_opSubF(const FuncGen* handle){
+	Board_LED_Set(LED0_B, TRUE);
+}
+
+
+/*! This function scan all EDU-CIAA-NXP buttons (TEC1, TEC2, TEC3 and TEC4),
+ *  and return ID of pressed button (TEC1 or TEC2 or TEC3 or TEC4)
+ *  or false if no button was pressed.
+ */
+uint32_t Buttons_GetStatus_(void) {
+	uint8_t ret = false;
+	uint32_t idx;
+
+	for (idx = 0; idx < 4; ++idx) {
+		if (gpioRead( TEC1 + idx ) == 0)
+			ret |= 1 << idx;
+	}
+	return ret;
+}
+
+
+/*! * This is a timed state machine that requires timer services
+ */
+
+/*! This function has to set up timers for the time events that are required
+ *  by the state machine.
+ */
+/*! This function will be called for each time event that is relevant for a
+ *  state when a state will be entered.
+ *  \param evid An unique identifier of the event.
+ *  \time_ms The time in milli seconds
+ *  \periodic Indicates the the time event must be raised periodically until
+ *   the timer is unset
+ */
+void funcGen_setTimer( FuncGen* handle, const sc_eventid evid, const sc_integer time_ms, const sc_boolean periodic )
+{
+	SetNewTimerTick(ticks, NOF_TIMERS, evid, time_ms, periodic);
+}
+
+/*! This function has to unset timers for the time events that are required
+ *  by the state machine.
+ */
+/*! This function will be called for each time event that is relevant for a
+ *  state when a state will be left.
+ *  \param evid An unique identifier of the event.
+ */
+void funcGen_unsetTimer( FuncGen* handle, const sc_eventid evid )
+{
+	UnsetTimerTick( ticks, NOF_TIMERS, evid );
+}
+
+
+/**
+ * @brief	Hook on Handle interrupt from SysTick timer
+ * @return	Nothing
+ */
+void SysTick_Handler(void){
+	SysTick_Time_Flag = true;
+}
+
+
+/**
+ * @brief	main routine for statechart example: EDU-CIAA-NXP - FuncGen LED3
+ * @return	Function should not exit.
+ */
+int main(void)
+{
+	uint32_t i;
+
+	uint32_t BUTTON_Status;
+
+	/* Generic Initialization */
+	Board_Init();
+
+	/* Init Ticks counter => TICKRATE_MS */
+	SysTick_Config(SystemCoreClock / TICKRATE_MS);
+
+	/* Init Timer Ticks */
+	InitTimerTicks( ticks, NOF_TIMERS );
+
+	/* Statechart Initialization */
+	funcGen_init( &statechart );
+	funcGen_enter( &statechart );
+
+	/* LED state is toggled in the main program */
+	while (1) {
+		/* The uC sleeps waiting for an interruption */
+		__WFI();
+
+		/* When a interrupt wakes to the uC, the main program validates it,
+		 * checking the waited Flag */
+		if (SysTick_Time_Flag == true) {
+
+			/* Then reset its Flag */
+			SysTick_Time_Flag = false;
+
+			/* Then Update all Timer Ticks */
+			UpdateTimers( ticks, NOF_TIMERS );
+
+			/* Then Scan all Timer Ticks */
+			for (i = 0; i < NOF_TIMERS; i++) {
+
+				/* Then if there are pending events */
+				if (IsPendEvent( ticks, NOF_TIMERS, ticks[i].evid ) == true) {
+
+					/* Then Raise an Event -> Ticks.evid => OK */
+					funcGen_raiseTimeEvent( &statechart, ticks[i].evid );
+
+					/* Then Mark as Attached -> Ticks.evid => OK */
+					MarkAsAttEvent( ticks, NOF_TIMERS, ticks[i].evid );
+				}
+			}
+			/* Then Get status of buttons */
+			BUTTON_Status = Buttons_GetStatus_();
+
+			/* Then if there are a pressed button */
+			if (BUTTON_Status != 0)
+
+				/* Then Raise an Event -> evTECXOprimodo => OK,
+				 * and Value of pressed button -> viTecla */
+				applicationIface_raise_evTECXOprimido(&statechart, BUTTON_Status);
+			else
+				/* Then else Raise an Event -> evTECXNoOprimido => OK */
+				applicationIface_raise_evTECXNoOprimido(&statechart);
+
+			/* Then Run an Cycle of Statechart */
+			funcGen_runCycle(&statechart);		// Run Cycle of Statechart
+		}
+	}
+}
+
+
+/** @} doxygen end group definition */
+
+/*==================[end of file]============================================*/
