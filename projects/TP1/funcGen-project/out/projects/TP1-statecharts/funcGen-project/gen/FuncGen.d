4aa0890efbeaf6241f94e3ee1c0f737e4fc371b7
Leapepe <leapalazzo@gmail.com>
2021-01-04 10:52:49 -0300

    reestructuracion de la carpeta project

---
+++ projects/TP1/funcGen-project/out/projects/TP1-statecharts/funcGen-project/gen/FuncGen.d
@@ -0,0 +1,5 @@
+projects/TP1-statecharts/funcGen-project/out/projects/TP1-statecharts/funcGen-project/gen/FuncGen.o: \
+ projects/TP1-statecharts/funcGen-project/gen/FuncGen.c \
+ projects/TP1-statecharts/funcGen-project/gen/FuncGen.h \
+ projects/TP1-statecharts/funcGen-project/gen/..\inc\sc_types.h \
+ projects/TP1-statecharts/funcGen-project/gen/FuncGenRequired.h
