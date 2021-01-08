e84f778a256dd3974e2bea64ea9c1fa5a9cd34bb
Leapepe <leapalazzo@gmail.com>
2021-01-08 11:00:59 -0300

    punto 3 funcionando

--- projects/TP3/3/inc/main.h
+++ projects/TP3/3/inc/main.h
@@ -7,4 +7,27 @@
 	COMMAND_LED4_TOGGLE = 'f'
 } commands_t;
 
+typedef enum{
+   TEC_ON,
+   TEC_OFF,
+   TEC_FALLING,
+   TEC_RISING
+} tecState_t;
+
+typedef struct {
+	gpioMap_t ID;
+	tecState_t st;
+	char* name;
+}tec_t;
+
+void tecInit(tec_t* t, gpioMap_t ID, char* name);
+
+void onTickUpdate();
+
+void tecUpdate(tec_t* t);
+
+void onTecRaised(tec_t* t);
+
+void onTecFalled(tec_t* t);
+
 void onUartRx(void* unused);
--- projects/TP3/3/src/main.c
+++ projects/TP3/3/src/main.c
@@ -1,11 +1,36 @@
 #include "main.h"
 
+//global variables
+tec_t tec1,tec2,tec3,tec4;
+
+
+// tick hook update callback function
+void onTickUpdate(){
+	tecUpdate(&tec1);
+	tecUpdate(&tec2);
+	tecUpdate(&tec3);
+	tecUpdate(&tec4);
+}
+
 // ---- Main function (entry point) ---- //
 int main(void)
 {
 	// Board initialization
 	boardConfig();
 	
+	// Tick rate configuration 50ms
+	tickConfig(50);
+
+	// Initialize tecs
+	tecInit(&tec1, TEC1, "TEC 1");
+	tecInit(&tec2, TEC2, "TEC 2");
+	tecInit(&tec3, TEC3, "TEC 3");
+	tecInit(&tec4, TEC4, "TEC 4");
+
+	// Generate callback on tick update
+   tickCallbackSet( onTickUpdate, NULL );
+
+
 	// uart initialization 8N1 115200
 	uartConfig( UART_USB, 115200 );
 
@@ -26,6 +51,7 @@ int main(void)
 }
 
 
+// uart callback on receive
 void onUartRx(void* unused){
 	uint8_t uartCharRx = uartRxRead(UART_USB);
 	switch(uartCharRx){
@@ -42,4 +68,59 @@ void onUartRx(void* unused){
 			gpioWrite(LED3,!gpioRead(LED3));
 			break;
 	}
+}
+
+
+
+// Initializes buttons structure
+void tecInit(tec_t* t, gpioMap_t ID, char* name){
+	t->st = TEC_OFF;
+	t->ID = ID;
+	t->name = name;
+}
+
+
+// buttons update function (state machine)
+// Buttons states are inverted
+void tecUpdate(tec_t* t){
+	switch(t->st){
+		case TEC_OFF:
+			if(gpioRead(t->ID) == LOW)
+				t->st = TEC_RISING;
+			break;
+
+		case TEC_ON:
+			if(gpioRead(t->ID) == HIGH)
+				t->st = TEC_FALLING;
+			break;
+
+		case TEC_RISING:
+			if(gpioRead(t->ID) == LOW){
+				t->st = TEC_ON;
+				onTecRaised(t);
+			} else
+				t->st = TEC_OFF;
+			break;
+
+		case TEC_FALLING:
+			if(gpioRead(t->ID) == HIGH){
+				t->st = TEC_OFF;
+				onTecFalled(t);
+			} else
+				t->st = TEC_ON;
+			break;
+	}
+}
+
+
+void onTecRaised(tec_t* t){
+	uartWriteString( UART_USB, "Tecla apretada: " );
+	uartWriteString( UART_USB, t->name );
+	uartWriteString( UART_USB, "\r\n" );
+}
+
+void onTecFalled(tec_t* t){
+	uartWriteString( UART_USB, "Tecla liberada: " );
+	uartWriteString( UART_USB, t->name );
+	uartWriteString( UART_USB, "\r\n" );
 }
